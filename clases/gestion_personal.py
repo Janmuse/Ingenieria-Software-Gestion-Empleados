7df5dbe2e0c0f0bdb773e64869dc64b979ffd4d9
arturobm389-sketch <arturobm389@gmail.com>
2025-12-02 22:05:26 -0600

    build(GestionPersonal): se crea la clase
    pendiente de implementar

---
+++ clases/gestion_personal.py
@@ -0,0 +1,8 @@
+class GestionPersonal:
+    def registrar_empleado(self, nombre, dpi, cargo, salario_hora):
+        # pendiente 
+        pass
+
+    def buscar_empleado_por_dpi(self, dpi):
+        # pendiente
+        pass.py

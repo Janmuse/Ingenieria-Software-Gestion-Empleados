2f6ec17fcbeb8166b5a8b507a59d3b0fb5210abe
Janmuse <jorge.and005@gmail.com>
2025-12-11 02:44:02 -0600

    chore(clases/gestion_personal.py): Se borra clase obsoleta

--- clases/gestion_personal.py
+++
@@ -1,8 +0,0 @@
-class GestionPersonal:
-    def registrar_empleado(self, nombre, dpi, cargo, salario_hora):
-        # pendiente 
-        pass
-
-    def buscar_empleado_por_dpi(self, dpi):
-        # pendiente
-        pass.py
